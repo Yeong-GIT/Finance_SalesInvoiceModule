71691d354dd1472c5b727c730854051496581d90
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-22 16:51:13 +0800

    feat(ui): Integrate data table ui. - Sales Invoice modified by data table plugin.
    Issues : Update bug

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'datatables.net',
+        'datatables.net-responsive',
+        'datatables.net-dt/css/jquery.dataTables.min.css',
+        'datatables.net-responsive-dt/css/responsive.dataTables.min.css'
+      ]
+    }
+  }
 })
