8a8b380cec8ce9d02acc37ff9564d7f5819336eb
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-29 03:28:32 +0800

    feat(kafka producer - Sales Invoice): Update producer config - Setting up proper producer service template

--- src/main/java/gityeong/SalesInvoice/service/KafkaProducerService.java
+++ src/main/java/gityeong/SalesInvoice/service/KafkaProducerService.java
@@ -1,15 +1,26 @@
 package gityeong.SalesInvoice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public void sendMessage(String topic, Object message) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(topic, jsonMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            // Handle serialization error
+        }
     }
 }
